8f5cc00066a82ae80ae5199429a2ae1b95dc830d
Marina Eremina <eremina.jobmail@gmail.com>
2019-01-11 14:42:35 +0100

    modify js file

--- src/js/logic.js
+++ src/js/logic.js
@@ -1,11 +1,17 @@
 (function() {
 
   //cache DOM, declare variables
-  const nav = document.querySelector('.navigation');
-  const question = document.querySelectorAll('.question');
+  const resultButton = document.querySelector('.js-result-button');
+  const result = document.querySelector('.js-result');
+  const questionNumber = 6;
+  let counterRight = 0;
+  let counterWrong = 0;
 
   //bind events
   document.addEventListener('click', movePrevNext);
+  document.addEventListener('click', processAnswer);
+  resultButton.addEventListener('click', showResult);
+  document.addEventListener('click', clearMessage);
 
   //function declarations
   function movePrevNext(event) {
@@ -22,7 +28,7 @@
       }
     }
     if (event.target.classList.contains('next')) {
-      if (currentQuestion.classList.contains('question-six')) {
+      if (currentQuestion.classList.contains('question-result')) {
         return;
       } else {
         let nextQuestion = currentQuestion.nextElementSibling;
@@ -31,6 +37,58 @@
       }
     }
   }
+
+  function processAnswer(event) {
+
+    let selectedAnswer = event.target;
+    
+    if (selectedAnswer.tagName == 'SPAN') {
+
+      let currentQuestion = document.querySelector('.question-is-active');
+
+      if (!currentQuestion.classList.contains('done')) {    
+        
+        if (selectedAnswer.classList.contains('js-correct')) {
+          selectedAnswer.classList.add('right');
+          counterRight = counterRight + 1;
+        } else {
+          let correctAnswer = currentQuestion.querySelector('span.js-correct');
+          correctAnswer.classList.add('right');
+          selectedAnswer.classList.add('wrong');
+          counterWrong = counterWrong + 1;
+        }
+
+        currentQuestion.classList.add('done');
+      }
+    }
+  }
+  //show result or info that not all questions have been answered
+  function showResult() {
+    let totalCount = counterRight + counterWrong;
+
+    result.innerHTML = (totalCount === questionNumber) 
+    ? `Wynik: ${counterRight} z ${questionNumber}.`
+    : `Please answer all questions.`;
+    
+  }
+  
+  //remove message after all questions have been answered
+  function clearMessage(event) {
+    let totalCount = counterRight + counterWrong;    
+    
+    if (event.target.tagName == 'SPAN' && totalCount === questionNumber && result.innerHTML === "Please answer all questions.") {
+      result.innerHTML = '';
+    }
+  }
+
+
+
+
+
+
+
+
+
 
 
 })();
